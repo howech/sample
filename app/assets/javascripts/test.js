a29677cd138c369b6d318b7225769d9246d35325
chris <chris@howeville.com>
2013-01-12 11:56:05 -0600

    minor changes

---
+++ app/assets/javascripts/test.js
@@ -0,0 +1,5 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+function this_is_a_test(x) {
+    console.log(x);
+}
